7429146c2fd51c2b3ac526f1c3462d7c2ca11f15
Guillaume <guillaume@MBP12.local>
2024-10-03 14:11:16 +0200

    add toggle on lessonDone function

--- src/rive.js
+++ src/rive.js
@@ -200,8 +200,8 @@ const lessonCounter = () => {
 	return total;
 };
 
-function lessonNdone(n) {
-	result = true;
+function lessonNdone(n, status) {
+	result = status;
 	riveInstance.setBooleanStateAtPath(`isDone${n}`, result, "compteur");
 	lessonsDone[n - 1] = result;
 	haloLessonActive[n - 1].value = result;
